3709e65c9700fb38b2dc40df4f3eb784d6489dd0
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2023-02-26 23:34:22 +0800

    Added new class for parsing in-game notices
    Note: The in-game notices are basically just html pages, and there is no encryption/decryption required, unlike the web API

---
+++ api/NoticesParser.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+from urllib import request
+import re
+from datetime import datetime
+
+class NoticesParser():
+    BASE_URL = 'https://api-sinoalice-us.pokelabo.jp'
+    NOTICES_ENDPOINT = '/web/announce/index?countryCode=36&languageType=2'
+
+    def __init__():
+        pass
+
+    def get_gc_dates(self):
+        pass
+
+    def _get_href_with_str(self):
+        pass
+
+    def _get_prelim_dates(self):
+        pass
+
+    def _get_entry_dates(self):
+        pass
+
+    def _get_finals_dates(self):
+        pass
