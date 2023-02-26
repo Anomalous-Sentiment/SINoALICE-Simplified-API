3fc0af8a956c01ba5317a35bd45f4d1b4394f894
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2023-02-26 23:47:38 +0800

    Fixed incorrect reference to class variables

--- api/NoticesParser.py
+++ api/NoticesParser.py
@@ -12,10 +12,13 @@ class NoticesParser():
 
     def get_gc_dates(self):
         # Get the page html, and parse as soup
-        notices_page_soup = BeautifulSoup(request.urlopen(BASE_URL + NOTICES_ENDPOINT).read(), 'html.parser')
+        notices_page_soup = BeautifulSoup(request.urlopen(NoticesParser.BASE_URL + NoticesParser.NOTICES_ENDPOINT).read(), 'html.parser')
 
         # Get the href of the element which links to the Gran Colo notice page
-        colo_page = self._get_href_with_str(notices_page_soup)
+        colo_page_href = self._get_href_with_str(notices_page_soup)
+
+        # Concaternate with base url for full page url
+        full_gc_page_url = NoticesParser.BASE_URL + colo_page_href
 
         pass
 
