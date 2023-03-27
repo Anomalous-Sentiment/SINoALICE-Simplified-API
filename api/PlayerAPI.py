015fcb1eab11423161a4b92b1188a48fcf5fbd4c
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2023-03-27 17:16:54 +0800

    Refactored PlayerAPI to separate the data obtained from the player profile, and the guild member list into different functions

--- api/PlayerAPI.py
+++ api/PlayerAPI.py
@@ -3,13 +3,14 @@ from .GuildAPI import GuildAPI
 import asyncio
 import aiohttp
 
+import json
 class PlayerAPI(GuildAPI):
     PLAYER_DATA_ENDPOINT = '/api/profile/get_alice_target_profile_data'
 
     def __init__(self):
         GuildAPI.__init__(self)
 
-    def get_players(self, guild_list = None):
+    def get_players_in_guilds(self, guild_list = None):
         # This function currently depends on using the guild list to retrieve a list of players
         # This is a flawed approach as not all guilds are returned in the call, and not all players are in a guild
         # But this is the only approach I can think of for now aside from brute forcing it and trying every possible
@@ -22,6 +23,7 @@ class PlayerAPI(GuildAPI):
         player_list = asyncio.run(self._get_players_main(guild_list))
 
         return player_list
+
 
     async def get_player_data(self, user_id):
         player_req_payload = {
@@ -51,32 +53,13 @@ class PlayerAPI(GuildAPI):
             member_res_list = await asyncio.gather(*[self._async_post(GuildAPI.GUILD_MEMBERS_ENDPOINT, payload, session) for payload in member_req_payloads])
 
             # Add the member list to the player list
-            for res in member_res_list:
+            for idx, res in enumerate(member_res_list):
                 # Check if there is a payload
                 if 'payload' in res:
                     player_list.extend(res['payload']['guildMemberList'])
                 else:
                     # To implement error handling. Failed to get list for some reason
-                    print('Failed to get member list: ', str(res))
-
-            # Create a list of payloads for getting player data of every player in list
-            for player in player_list:
-                new_player_data_payload = {
-                    'targetUserId': player['guildUserData']['userId']
-                }
-
-                player_data_payloads.append(new_player_data_payload)
-
-            # Split requests into chucks to avoid disconnect from server
-            for chunk in self._chunks(player_data_payloads, 1000):
-                # For each member, call the API endpoint to get their player data
-                temp_list = await asyncio.gather(*[self._async_post(PlayerAPI.PLAYER_DATA_ENDPOINT, payload, session) for payload in chunk])
-                player_data_res_list.extend(temp_list)
-
-            # Combine the data
-            for guild_player_data, player_res_data in zip(player_list, player_data_res_list):
-                # Combine dicts
-                guild_player_data['userData'].update(player_res_data['payload'])
+                    print('Failed to get member list: ', str(res), '. Guild id: ', str(guild_list[idx]['guildDataId']), '. Guild name: ', str(guild_list[idx]['guildName']))
 
         return player_list
 
@@ -85,3 +68,31 @@ class PlayerAPI(GuildAPI):
         """Yield successive n-sized chunks from lst."""
         for i in range(0, len(lst), n):
             yield lst[i:i + n]
+
+    async def _get_basic_player_info_main(self, player_id_list):
+        player_data_res_payload_list = []
+        player_data_payloads = []
+
+        # Create a list of payloads for getting player data of every player in list
+        for player_id in player_id_list:
+            new_player_data_payload = {
+                'targetUserId': player_id
+            }
+
+            player_data_payloads.append(new_player_data_payload)
+
+        async with aiohttp.ClientSession(BaseAPI.URL) as session:
+            # Split requests into chucks to avoid disconnect from server
+            for chunk in self._chunks(player_data_payloads, 1000):
+                # For each member, call the API endpoint to get their player data
+                temp_list = await asyncio.gather(*[self._async_post(PlayerAPI.PLAYER_DATA_ENDPOINT, payload, session) for payload in chunk])
+
+                # Add the payloads of each res body to the list
+                for res in temp_list:
+                    player_data_res_payload_list.append(res['payload'])
+
+        return player_data_res_payload_list
+
+    def get_basic_player_info(self, player_list=[]):
+        basic_player_data_list = asyncio.run(self._get_basic_player_info_main(player_list))
+        return basic_player_data_list
